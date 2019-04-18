77ba5a15fd29a1269b1a7ec0f35da639335f21b2
bgpark82 <bgpark82@gmail.com>
2019-04-18 23:54:24 +0900

    로컬 환경 완료

--- src/main/java/com/aws/webservice/web/WebRestController.java
+++ src/main/java/com/aws/webservice/web/WebRestController.java
@@ -1,13 +1,30 @@
 package com.aws.webservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aws.webservice.domain.posts.PostsRepository;
+import com.aws.webservice.dto.posts.PostsSaveRequestDto;
+import com.aws.webservice.service.PostsService;
+
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
 
+	private PostsRepository postsRepository;
+	private PostsService postsService;
+	
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+    
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
 }
--- src/test/java/com/aws/webservice/ApplicationTests.java
+++ src/test/java/com/aws/webservice/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aws;
+package com.aws.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
